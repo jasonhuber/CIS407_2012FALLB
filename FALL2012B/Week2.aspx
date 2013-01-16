8df24fb7d58c9e8dbc38c8f90dfa84bd32776424
Jason Huber <JasonHuber@gmail.com>
2013-01-15 20:44:40 -0700

    Added week two goodness..

---
+++ FALL2012B/Week2.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Week2.aspx.cs" Inherits="FALL2012B.Week2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="pnlInput" runat="server">
+        First Name : <asp:TextBox runat="server" ID="txtFname"></asp:TextBox><br />
+        Last Name : <asp:TextBox runat="server" ID="txtLname"></asp:TextBox><br />
+        <asp:Button runat="server" ID="cmdInput" Text="Submit" 
+            onclick="cmdInput_Click" /> <asp:Button runat="server" ID="cmdCancel" Text="Cancel" />
+    </asp:Panel>
+
+    <asp:Panel ID="pnlOutput" runat="server">
+        <asp:TextBox runat="server" ID="txtOutput" Height="288px" Width="365px" 
+            TextMode="MultiLine"></asp:TextBox><br />
+        <asp:Button runat="server" ID="btnRedo" Text="Redo" onclick="btnRedo_Click" />
+    </asp:Panel>
+
+</asp:Content>
---
+++ FALL2012B/Week2.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FALL2012B
+{
+    public partial class Week2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["debug"] != "true")
+            {
+                pnlOutput.Visible = false;   
+            }
+            if (Request.Params["lastname"] != null)
+            {
+                txtLname.Text = Request.Params["lastname"];
+            }
+        }
+
+        protected void cmdInput_Click(object sender, EventArgs e)
+        {
+            pnlOutput.Visible = true;
+            pnlInput.Visible = false;
+
+            txtOutput.Text = txtFname.Text + "\n" + txtLname.Text;
+        }
+
+        protected void btnRedo_Click(object sender, EventArgs e)
+        {
+            pnlOutput.Visible = false;
+            pnlInput.Visible = true;
+
+            foreach (Control item in Form.Controls)
+            {
+                if (item is ContentPlaceHolder)
+                {
+                    foreach (Control pnl in item.Controls)
+                    {
+                        if (pnl is Panel)
+                        {
+                            foreach (Control txt  in pnl.Controls)
+                            {
+                                if (txt is TextBox)
+                                {
+                                    ((TextBox)txt).Text = "";
+                                }    
+                            }
+                            
+                        }
+                    }//end for each placeholder
+                }
+            }//end foreach controls (the upper most one)
+            //this is only for Matthew
+            ClientScript.RegisterClientScriptBlock(this.GetType(),"23","<script>alert('Matthew was here')</script>");
+            
+        }
+    }
+}
